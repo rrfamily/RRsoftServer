efdae406fbebe1766c03af507e28f9c3f97f0fb0
rrfamily <rrfamilyofficial1@gmail.com>
2020-06-30 03:51:40 +0300

    added edit and remove option

--- app/src/main/java/ras/dev/rrsoftserver/ProductList.java
+++ app/src/main/java/ras/dev/rrsoftserver/ProductList.java
@@ -17,6 +17,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -305,6 +306,162 @@ public class ProductList extends AppCompatActivity {
 
         }
     }
+
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        if (item.getTitle().equals(Common.UPDATE)){
+
+            showUpdateProductDialog(adapter.getRef(item.getOrder()).getKey(),adapter.getItem(item.getOrder()));
+
+        }
+        else if(item.getTitle().equals(Common.DELETE)){
+
+           deleteProduct(adapter.getRef(item.getOrder()).getKey());
+
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    private void deleteProduct(String key) {
+        productList.child(key).removeValue();
+
+    }
+
+    private void showUpdateProductDialog(final String key, final Product item) {
+
+        AlertDialog.Builder alertDiaglog = new AlertDialog.Builder(ProductList.this);
+        alertDiaglog.setTitle("Edit Product");
+        alertDiaglog.setMessage("Please fill information");
+
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View add_menu_layout = inflater.inflate(R.layout.add_new_product_layout,null);
+
+        edtName = add_menu_layout.findViewById(R.id.edtName);
+        edtDescription = add_menu_layout.findViewById(R.id.edtDescription);
+        edtPrice = add_menu_layout.findViewById(R.id.edtPrice);
+        edtDiscount = add_menu_layout.findViewById(R.id.edtDiscount);
+        btnSelect = add_menu_layout.findViewById(R.id.btnSelect);
+        btnUpload = add_menu_layout.findViewById(R.id.btnUpload);
+
+        //set Default Value for View
+        edtName.setText(item.getName());
+        edtDiscount.setText(item.getDiscount());
+        edtDescription.setText(item.getDescription());
+        edtPrice.setText(item.getPrice());
+
+        //Event for Button
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage(); //let user select image form gallery and safe url of the image
+            }
+        });
+
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImage(item);
+            }
+        });
+
+
+
+
+        alertDiaglog.setView(add_menu_layout);
+        alertDiaglog.setIcon(R.drawable.ic_shopping_cart_black_24dp);
+
+
+        //Set Button
+        alertDiaglog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.dismiss();
+
+                //here we create new catogery
+
+
+                    //Update Info
+                    item.setName(edtName.getText().toString());
+                    item.setPrice(edtPrice.getText().toString());
+                    item.setDiscount(edtDiscount.getText().toString());
+                    item.setDescription(edtDescription.getText().toString());
+
+
+
+
+                    productList.child(key).setValue(item);
+                    Snackbar.make(rootLayout," Product "+item.getName()+"Edited",Snackbar.LENGTH_SHORT).show();
+
+
+
+            }
+        });
+        alertDiaglog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.dismiss();
+
+            }
+        });
+        alertDiaglog.show();
+
+    }
+
+    private void changeImage(final Product item) {
+
+        if(saveUri != null){
+
+            final ProgressDialog mDialog = new ProgressDialog(this);
+            mDialog.setMessage("Uploading....");
+            mDialog.show();
+
+            String imageName = UUID.randomUUID().toString();
+            final StorageReference imageFolder = storageReference.child("images/"+imageName);
+            imageFolder.putFile(saveUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    mDialog.dismiss();
+                    Toast.makeText(ProductList.this,"Uploaded",Toast.LENGTH_SHORT).show();
+                    imageFolder.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            //set value for newcatorgey if image upload and we can get download link
+                            item.setImage(uri.toString());
+
+
+                        }
+                    });
+
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    mDialog.dismiss();
+                    Toast.makeText(ProductList.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onProgress(@NonNull UploadTask.TaskSnapshot taskSnapshot) {
+                    double progress = (100.0 * taskSnapshot.getBytesTransferred()/taskSnapshot.getTotalByteCount());
+                    mDialog.setMessage("Uploaded"+progress+ "%");
+
+                }
+            });
+
+        }
+
+
+    }
+
+
+
 }
 
 
